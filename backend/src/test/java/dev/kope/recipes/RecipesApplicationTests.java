5bde75c6397fafd52db496117eb489e7ce4806b6
kmatotek <km998744@wcupa.edu>
2026-08-31 08:49:45 -0400

    added backend initialization

---
+++ backend/src/test/java/dev/kope/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kope.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
