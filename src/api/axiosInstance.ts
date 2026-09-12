6396d994fa363b84743ec0c98651671ff9a9bda0
kmatotek <km998744@wcupa.edu>
2026-09-12 15:08:55 -0400

    udpate axios url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+
+  baseURL: import.meta.env.VITE_API_URL,
+
   timeout: 10000, // 10 seconds timeout
+
   headers: {
+
     'Content-Type': 'application/json',
+
     'Accept': 'application/json',
+
   },
+
 });
 
 export default api;
